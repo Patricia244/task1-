177150255376a122c5a0d4465d06e99524dd66a8
Silindile <patrciaslindile5@gmail.com>
2021-08-16 03:28:08 +0100

    some of all mutiples of 3 or 5 below 1000

---
+++ task1.1.js
@@ -0,0 +1,12 @@
+function multiplesof3And5(num){
+        var sum = 0;
+        for (var i = 1 ; i < num; i++){
+        if(!(i % 3)|| !(i % 5)){
+            sum += i
+        }
+    }
+    return sum
+}
+
+
+multiplesof3And5(1000);
